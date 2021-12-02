aad7020ed8f927f89697f1f605b3b38f6eb6a024
Amaral1973 <61523977+Amaral1973@users.noreply.github.com>
2021-12-02 13:55:21 -0300

    Atualização 02/12
    Arquivos refeitos e atualizados.

---
+++ FrmBuscaCEP.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace CircodeAppsTDS06
+{
+    public partial class FrmBuscaCEP : Form
+    {
+        public FrmBuscaCEP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + txtCEP.Text + "/json");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show("Servidor Indisponível!");
+                return; //Sai da rotina e para e codificação
+            }
+            using (Stream webStream = ChecaServidor.GetResponseStream())
+            {
+                if (webStream != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(webStream))
+                    {
+                        string response = responseReader.ReadToEnd();
+                        response = Regex.Replace(response, "[{},]", string.Empty);
+                        response = response.Replace("\"", "");
+
+                        String[] substrings = response.Split('\n');
+
+                        int cont = 0;
+                        foreach (var substring in substrings)
+                        {
+                            if (cont == 1)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                if (valor[0] == "  erro")
+                                {
+                                    MessageBox.Show("CEP não encontrado!");
+                                    txtCEP.Focus();
+                                    return;
+                                }
+                            }
+
+                            //Endereço
+                            if (cont == 2)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblEndereco2.Text = valor[1];
+                            }
+
+                            //Complemento
+                            if (cont == 3)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblComplemento2.Text = valor[1];
+                            }
+
+                            //Bairro
+                            if (cont == 4)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblBairro2.Text = valor[1];
+                            }
+
+                            //Cidade
+                            if (cont == 5)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblCidade2.Text = valor[1];
+                            }
+                            cont++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace CircodeAppsTDS06
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblPedraPapelTesoura = new System.Windows.Forms.Label();
+            this.pbxJogo = new System.Windows.Forms.PictureBox();
+            this.pbxCEP = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxJogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCEP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCEP.Location = new System.Drawing.Point(47, 156);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(70, 13);
+            this.lblCEP.TabIndex = 0;
+            this.lblCEP.Text = "Busca CEP";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(522, 288);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(112, 42);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblPedraPapelTesoura
+            // 
+            this.lblPedraPapelTesoura.AutoSize = true;
+            this.lblPedraPapelTesoura.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPedraPapelTesoura.Location = new System.Drawing.Point(181, 156);
+            this.lblPedraPapelTesoura.Name = "lblPedraPapelTesoura";
+            this.lblPedraPapelTesoura.Size = new System.Drawing.Size(142, 13);
+            this.lblPedraPapelTesoura.TabIndex = 3;
+            this.lblPedraPapelTesoura.Text = "Pedra - Papel - Tesoura";
+            // 
+            // pbxJogo
+            // 
+            this.pbxJogo.Image = global::CircodeAppsTDS06.Properties.Resources.pedrapapeltesoura1;
+            this.pbxJogo.Location = new System.Drawing.Point(180, 12);
+            this.pbxJogo.Name = "pbxJogo";
+            this.pbxJogo.Size = new System.Drawing.Size(143, 141);
+            this.pbxJogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxJogo.TabIndex = 4;
+            this.pbxJogo.TabStop = false;
+            this.pbxJogo.Click += new System.EventHandler(this.pbxJogo_Click);
+            // 
+            // pbxCEP
+            // 
+            this.pbxCEP.Image = global::CircodeAppsTDS06.Properties.Resources.cep;
+            this.pbxCEP.Location = new System.Drawing.Point(12, 12);
+            this.pbxCEP.Name = "pbxCEP";
+            this.pbxCEP.Size = new System.Drawing.Size(143, 141);
+            this.pbxCEP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCEP.TabIndex = 1;
+            this.pbxCEP.TabStop = false;
+            this.pbxCEP.Click += new System.EventHandler(this.pbxCEP_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(646, 342);
+            this.Controls.Add(this.pbxJogo);
+            this.Controls.Add(this.lblPedraPapelTesoura);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.pbxCEP);
+            this.Controls.Add(this.lblCEP);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Circo de Apps";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxJogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCEP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCEP;
+        private System.Windows.Forms.PictureBox pbxCEP;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.PictureBox pbxJogo;
+        private System.Windows.Forms.Label lblPedraPapelTesoura;
+    }
+}
+
